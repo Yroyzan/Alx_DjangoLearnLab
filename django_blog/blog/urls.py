923ce6422e1e3897077d96c6587c37e6b0b69809
ZaynSol <zaynsolomonmar@gmail.com>
2025-08-12 18:59:48 +0300

    Issue fixed

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,23 +1,16 @@
 from django.urls import path
-from . import views
-from django.contrib.auth import views as auth_views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, add_comment, CommentUpdateView, CommentDeleteView, PostByTagListView, search_posts
-
+from .views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+    PostUpdateView,
+    PostDeleteView,
+)
 
 urlpatterns = [
-    path("register/", views.register, name="register"),
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
-    path("profile/", views.profile, name="profile"),
-    path("post/", PostListView.as_view(), name="post_list"),
-    path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
-    path("post/new/", PostCreateView.as_view(), name="post_new"),
-    path("post/<int:pk>/Update/", PostUpdateView.as_view(), name="post_edit"),
-    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
-    path('post/<int:pk>/comments/new/', add_comment, name='add_comment'),
-    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_edit'),
-    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
-    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='tag_posts'),
-    path('search/', search_posts, name='search_posts'),
+    path("", BlogListView.as_view(), name="home"),
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="post-detail"),
+    path("post/new/", BlogCreateView.as_view(), name="post-new"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
 ]
