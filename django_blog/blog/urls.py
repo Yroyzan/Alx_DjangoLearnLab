6f263b73f553ab566d875eba4284a8c470256598
ZaynSol <zaynsolomonmar@gmail.com>
2025-08-13 05:49:43 +0300

    Issue fixed

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import (
     PostListView, PostDetailView, PostCreateView, 
-    PostUpdateView, PostDeleteView, search_posts
+    PostUpdateView, PostDeleteView
 )
+from .views import search_posts  # Separate import for the search feature
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
@@ -10,5 +11,5 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('search/', search_posts, name='post-search'),  # <-- Added for new feature
+    path('search/', search_posts, name='post-search'),
 ]
